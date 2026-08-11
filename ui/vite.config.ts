cf57ff611fc9be82817bb497efe15603d6ffcf09
Yinuo Huang <yinuo_huang@med.unc.edu>
2026-08-11 10:34:40 -0400

    configure ui

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: [
+      "https://study-palette-client-dept-bdc-data-portal.apps.cloudapps.unc.edu",
+    ],
     port: 3000,
     proxy: {
       "/api": {
