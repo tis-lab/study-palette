a121cc0a4fc19e07a0c388b26d5906ba5dd7fdb8
yn-huang <100048534+yn-huang@users.noreply.github.com>
2026-08-19 17:11:32 -0400

    Configure for Cloudapps deployment
    Co-authored-by: Yinuo Huang <yinuo_huang@med.unc.edu>

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: process.env.VITE_ALLOWED_HOST
+      ? process.env.VITE_ALLOWED_HOST.split(",").map((host) => host.trim())
+      : [],
     port: 3000,
     proxy: {
       "/api": {
